30f84c4ad4c7ce5457eae5044aa7bc1b8452bc97
Martijn Visser <m.visser6@students.uu.nl>
2018-02-10 18:50:53 +0100

    pong

---
+++ GameEngine/ContentLoader.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameEngine
+{
+    public static class ContentLoader
+    {
+        public static ContentManager Content { get; set; }
+
+        private static Dictionary<string, Texture2D> textures = new Dictionary<string,Texture2D>();
+
+        public static Texture2D LoadSprite(string source)
+        {
+            Texture2D texture;
+
+            if(!textures.Keys.Contains(source))
+            {
+                texture = Content.Load<Texture2D>(source);
+                textures.Add(source, texture);
+            }
+            else
+            {
+                return textures[source];
+            }
+
+            return texture;
+        }
+    }
+}
---
+++ GameEngine/GameObject.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace GameEngine
+{
+    public class GameObject
+    {
+        public Vector2 Position { get; set; }
+        public Vector2 Velocity { get; set; }
+        private Texture2D _mTexture;
+
+
+
+        public GameObject(Texture2D texture, Vector2 position)
+        {
+            _mTexture = texture;
+            Position = position;
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            Position += Velocity * ((gameTime.ElapsedGameTime.Milliseconds / 1000.0f) * 60);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_mTexture, Position, Color.White);
+        }
+
+        public Rectangle GetBoundingBox()
+        {
+            return new Rectangle((int)Position.X, (int)Position.Y, _mTexture.Width, _mTexture.Height);
+        }
+
+        public bool CheckCollision(GameObject obj)
+        {
+            if (this.GetBoundingBox().Intersects(obj.GetBoundingBox()))
+                return true;
+
+            return false;
+        }
+    }
+}

c3f1d5069081b12ea040c30d6f6555154e8b7cfa
sam1994nixon <33206657+sam1994nixon@users.noreply.github.com>
2018-02-10 18:32:45 +0000

    bats and movement

---
+++ Pong/Pong/Pong/Bat.cs
@@ -0,0 +1,17 @@
+﻿using GameEngine;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pong
+{
+    public class Bat : GameObject
+    {
+        public Bat(Vector2 position) : base(ContentLoader.LoadSprite("bat"), position)
+        {
+
+        }
+    }
+}
--- Pong/Pong/Pong/Game1.cs
+++ Pong/Pong/Pong/Game1.cs
@@ -8,6 +8,7 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using GameEngine;
 
 namespace Pong
 {
@@ -17,17 +18,27 @@ namespace Pong
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;        
+        SpriteBatch spriteBatch;
+        Bat player1, player2;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
-        
+
+        protected override void Initialize()
+        {           
+            base.Initialize();
+        }
+
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            ContentLoader.Content = Content;
+            player1 = new Bat(new Vector2(0, 300));
+            player2 = new Bat(new Vector2(768, 300));
         }
       
         protected override void Update(GameTime gameTime)
@@ -36,6 +47,9 @@ namespace Pong
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
 
+            player1.Update(gameTime);
+            player2.Update(gameTime);
+            MoveBats();
             base.Update(gameTime);
         }
 
@@ -44,7 +58,33 @@ namespace Pong
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            spriteBatch.Begin();
+            player1.Draw(spriteBatch);
+            player2.Draw(spriteBatch);
+            spriteBatch.End();
             base.Draw(gameTime);
+        }
+
+        private void MoveBats()
+        {
+            KeyboardState ks = Keyboard.GetState();
+            if(ks.IsKeyDown(Keys.W))
+            {
+                player1.Position += new Vector2(0, -3);
+            }
+            if (ks.IsKeyDown(Keys.S))
+            {
+                player1.Position += new Vector2(0, 3);
+            }
+
+            if (ks.IsKeyDown(Keys.Up))
+            {
+                player2.Position += new Vector2(0, -3);
+            }
+            if (ks.IsKeyDown(Keys.Down))
+            {
+                player2.Position += new Vector2(0, 3);
+            }
         }
     }
 }
